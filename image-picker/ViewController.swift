c0959567414ffd186bc07951294f2da6e31df753
Aqib Shah <ashah023@fiu.edu>
2017-04-16 23:47:15 -0400

    Initial Commit

---
+++ image-picker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  image-picker
+//
+//  Created by Aqib Shah on 4/16/17.
+//  Copyright © 2017 Aqib Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
