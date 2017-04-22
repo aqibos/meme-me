b21e0430b538b85cbabc69347764e3160a5518f2
Aqib Shah <ashah023@fiu.edu>
2017-04-22 10:48:44 -0400

    WIP: Revision #2

--- image-picker/MemeEditorViewController.swift
+++ image-picker/MemeEditorViewController.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
+  
+  // MARK: Outlets and Variables
   
   @IBOutlet weak var imageView: UIImageView!
   @IBOutlet weak var cameraButton: UIBarButtonItem!
@@ -17,23 +19,23 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   @IBOutlet weak var toolbar: UIToolbar!
   @IBOutlet weak var shareButton: UIBarButtonItem!
   @IBOutlet weak var navbar: UINavigationBar!
-
+  
   let memeTextAttributes: [String: Any] = [
     NSStrokeColorAttributeName: UIColor.black,
     NSForegroundColorAttributeName: UIColor.white,
     NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
     NSStrokeWidthAttributeName: -4
   ]
-    
+  
   // MARK: Lifecycle Methods
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     configureTextField(topTextField, withText: "TOP")
     configureTextField(bottomTextField, withText: "BOTTOM")
     shareButton.isEnabled = false
   }
-    
+  
   override func viewWillAppear(_ animated: Bool) {
     super.viewWillAppear(animated)
     cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
@@ -44,19 +46,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     super.viewWillDisappear(animated)
     unsubscribeFromKeyboardNotifications()
   }
-
+  
   func configureTextField(_ textField: UITextField, withText: String = "") {
     textField.delegate = self
     textField.defaultTextAttributes = memeTextAttributes
     textField.text = withText
     textField.textAlignment = .center
   }
-
+  
   // MARK: Actions
   
   @IBAction func pickImageFromGallery() { pickImage(sourceType: .photoLibrary) }
   @IBAction func pickImageFromCamera() { pickImage(sourceType: .camera) }
-
+  
   func pickImage(sourceType: UIImagePickerControllerSourceType) {
     let imagePickerCtrl = UIImagePickerController()
     imagePickerCtrl.delegate = self
@@ -69,14 +71,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     bottomTextField.text = "BOTTOM"
     imageView.image = nil
   }
-
-  @IBAction func saveMeme() {
+  
+  // TODO: Have separate method that creates an instance of Meme struct
+  //       Then, call this method from 'completionWithItemsHandler'
+  
+  // Save generated meme to gallery (device storage)
+  
+  func saveMeme(memedImage: UIImage) {
+    UIImageWriteToSavedPhotosAlbum(memedImage, nil, nil, nil)
+  }
+  
+  
+  // I don't really understand the expectations for this
+  // As stated per Apple's documentation, the completionWithItemsHandler
+  //   is called after the UIActivityViewController is dismissed. In other words,
+  //   the image will already have been saved to the camera roll. Why must we 
+  //   save the image again, and manually dismiss the UIActivityViewController, if 
+  //   it is already being handled?
+  @IBAction func shareMeme(_ sender: Any) {
     let meme = Meme(topText: topTextField.text!,
                     bottomText: bottomTextField.text!,
                     originalImage: imageView.image!,
                     memedImage: generateMemedImage())
     
     let activityViewCtrl = UIActivityViewController(activityItems: [meme.memedImage], applicationActivities: nil)
+    activityViewCtrl.completionWithItemsHandler = {(activityType: UIActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) -> Void in
+      if (completed) {
+        self.saveMeme(memedImage: meme.memedImage)
+        self.dismiss(animated: true, completion: nil)
+      }
+    }
     present(activityViewCtrl, animated: true, completion: nil)
   }
   
@@ -102,7 +126,7 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   
   func imagePickerController(_ picker: UIImagePickerController,
                              didFinishPickingMediaWithInfo info: [String : Any]) {
-
+    
     if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
       imageView.image = image
       imageView.contentMode = .scaleAspectFit
@@ -121,8 +145,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
   
   func textFieldDidBeginEditing(_ textField: UITextField) {
     // Tag 0 refers top UITextField, and Tag 1 refers to bottom UITextField
-    // Clear the text every time... 
-      textField.text = ""
+    // Clear the text every time...
+    textField.text = ""
   }
   
   // MARK: Handle UIKeyboard
@@ -152,5 +176,5 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
     NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
   }
-
-}   
+  
+}
